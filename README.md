1212624e484c31dba36a4e175d8bb4f788dffbb9
Kostas Hatalis <hatalis@gmail.com>
2018-08-30 20:26:33 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# ![](images/1.PNG?raw=true "Icon")
+
+A development framework for demand side management research.
+
+## Installation
+
+Clone/fork this repository and then run:
+```
+python setup.py develop
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='lehighdsm',
+      version='0.1',
+      description='Package for DSM project.',
+      url='',
+      author='Kostas Hatalis',
+      author_email='',
+      license='',
+      packages=['lehighdsm'],
+      zip_safe=False,
+      install_requires=[]
+      )
